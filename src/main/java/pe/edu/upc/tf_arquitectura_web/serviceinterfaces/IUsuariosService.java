08ed51b1833c7434a69f3e1bf8e1494870cc1a1f
YaneluC <99844598+YaneluC@users.noreply.github.com>
2023-09-20 19:02:04 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/tf_arquitectura_web/serviceinterfaces/IUsuariosService.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.tf_arquitectura_web.serviceinterfaces;
+
+import pe.edu.upc.tf_arquitectura_web.entities.Usuarios;
+
+import java.util.List;
+
+public interface IUsuariosService {
+    public void insert(Usuarios usuarios);
+    public List<Usuarios> list();
+    public void delete(int idUsuarios);
+    public Usuarios listId(int id);
+    public List<String[]> ordensegunfechanacimiento();
+}
